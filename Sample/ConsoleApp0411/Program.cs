f8dedf53d92531b26dc6527ec394381b5dcc425d
infosys <infosys@C4209>
2023-04-17 10:06:59 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample/ConsoleApp0411/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0411 {
+    class Program {
+
+        static int num1;
+        static int num;
+        public static void money() {
+            for (int i = 0; i < num1; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args) {
+
+            Console.Write("金額:");
+            int sum = int.Parse(Console.ReadLine());
+            Console.Write("支払い");
+            int pay = int.Parse(Console.ReadLine());
+            int change = pay - sum;
+            Console.WriteLine("お釣り:" + change);
+            num1 = change / 10000;num = change % 10000;
+            Console.Write("一万円札:"); money();
+            num1 = num / 5000; num = num % 5000;
+            Console.Write("五千円札:"); money();
+            num1 = num / 2000; num = num % 2000;
+            Console.Write("二千円札:"); money();
+            num1 = num / 1000;num = num % 1000;
+            Console.Write("千円札:"); money();
+            num1 = num / 500;num = num % 500;
+            Console.Write("五百円:"); money();
+            num1 = num / 100;num = num % 100;
+            Console.Write("百円:"); money();
+            num1 = num / 50;num = num % 50;
+            Console.Write("五十円:"); money();
+            num1 = num / 10; num = num % 10;
+            Console.Write("十円:"); money();
+            num1 = num / 5;num = num % 5;
+            Console.Write("五円:"); money();
+            num1 = num / 1;
+            Console.Write("一円:"); money();
+        }
+    }
+}
---
+++ Sample/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btButton_Click(object sender, EventArgs e) {
+           int Ans = int.Parse(tbNum1.Text) + int.Parse(tbNum2.Text);
+            tbAns.Text = Ans.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
