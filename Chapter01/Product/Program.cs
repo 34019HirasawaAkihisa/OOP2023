1af2528623420bd27f7710ae34ffcf1a10085269
平澤　輝久 <OIBC34019@ojs.ac.jp>
2023-05-08 11:50:19 +0900

    0508演習1

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -8,11 +8,28 @@ namespace ProductSample {
 
     class Program {
         static void Main(string[] args) {
-            Product karinto = new Product(123, "かりんとう", 180);
-            Product daihuku = new Product(235, "大福もち", 160);
 
-            Console.WriteLine(karinto.GetPriceIncludingTax());
-            Console.WriteLine(daihuku.GetPriceIncludingTax());
+            #region  P26のサンプルプログラム
+            //Product karinto = new Product(123, "かりんとう", 180);
+            //Product daihuku = new Product(235, "大福もち", 160);
+
+            //Console.WriteLine(karinto.GetPriceIncludingTax());
+            //Console.WriteLine(daihuku.GetPriceIncludingTax());
+            #endregion
+
+            //DateTime data = new DateTime(2023, 5, 8);
+            DateTime date = DateTime.Today;
+            string str = date.ToString("yyyy年MM月dd日");
+
+            Console.WriteLine("今日の日付:" + str);
+
+            //10日後を求める
+            DateTime daysAfter10 = date.AddDays(10);
+            Console.WriteLine("10日後:" + daysAfter10.ToString("yyyy年MM月dd日") +  "です。");
+
+            //10日前を求める
+            DateTime daysBefor10 = date.AddDays(-10);
+            Console.WriteLine("10日前:" + daysBefor10.ToString("yyyy年MM月dd日") + "です。");
         }
     }
 }
