f5fdd685c770a710c279e668c00946def5a7e239
平澤　輝久 <OIBC34019@ojs.ac.jp>
2023-05-08 10:54:08 +0900

    P26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+
+    class Program {
+        static void Main(string[] args) {
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daihuku = new Product(235, "大福もち", 160);
+
+            Console.WriteLine(karinto.GetPriceIncludingTax());
+            Console.WriteLine(daihuku.GetPriceIncludingTax());
+        }
+    }
+}
