947dc1aad2bc15b04fd8aefab87140d6cda1612d
平澤　輝久 <OIBC34019@ojs.ac.jp>
2023-05-15 12:08:28 +0900

    初版の完成P68

--- Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
@@ -5,6 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SalesCalculator {
-    class SalesCounter {
+    public class SalesCounter {
+
+        private List<Sale> _sales;
+
+        //コンストラクタ
+        public SalesCounter(List<Sale> sales) {
+            _sales = sales;
+        }
+
+        //店舗別の売り上げを求める
+        public Dictionary<string, int> GetPerStoreSales() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount;　//店名が既に存在する
+                else
+                    dict[sale.ShopName] = sale.Amount; //店名が存在しない (新規格納)
+            }
+            return dict;
+        }
     }
 }
