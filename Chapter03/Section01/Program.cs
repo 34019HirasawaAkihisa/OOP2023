8fbf1153663d3309ff50d1fe430f37fedf55ae56
平澤　輝久 <OIBC34019@ojs.ac.jp>
2023-05-23 10:30:54 +0900

    P87まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
 
-        public delegate bool Judgement(int value); //デリゲートの宣言
-
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, 8, 6, 3, 5, 15, 17, 19, 23, 19, 16, 3 };
 
-            int count = numbers.Count(n => n % 2 == 0);
+            //int count = numbers.Count(n => n % 5 == 0 && n != 0);
+            //合計値
+            var sum = numbers.Where(n => n % 2 == 0).Average();
 
-            Console.WriteLine(count); ;
+            Console.WriteLine(sum); ;
         }
     }
 }
