0a267280f0f4882d6746297b91589a9dcf8a755d
平澤　輝久 <OIBC34019@ojs.ac.jp>
2023-06-27 16:58:51 +0900

    設問１_提出

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -11,32 +11,32 @@ namespace Test01 {
         }
 
 
-        //メソッドの概要： 
+        //メソッドの概要： 学生データを読み込み、Studentオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
-            var students = new List<Student>(); //売上データを格納する
+            var students = new List<Student>(); //学生データを格納する
             var lines = File.ReadAllLines(filePath);　//ファイルからすべてのデータを読み込む
 
             foreach (var line in lines) { //すべての行から一行ずつ取り出す
 
                 var items = line.Split(','); //区切りで項目別に分ける
-                var sale = new Student {　//Saleインスタンスを生成
+                var student = new Student {　//Studentインスタンスを生成
                     Name = items[0],
                     Subject = items[1],
                     Score = int.Parse(items[2]),
                 };
-                students.Add(sale); //Saleインスタンスをコレクションに追加
+                students.Add(student); //studentインスタンスをコレクションに追加
             }
             return students;
         }
 
-        //メソッドの概要： 生徒別の点数を求める
+        //メソッドの概要： 学生別の点数を求める
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new SortedDictionary<string, int>();
             foreach (Student student in _score) {
                 if (dict.ContainsKey(student.Name))
-                    dict[student.Name] += student.Score;　//店名が既に存在する
+                    dict[student.Name] += student.Score;　//学生名が既に存在する
                 else
-                    dict[student.Name] = student.Score; //店名が存在しない (新規格納)
+                    dict[student.Name] = student.Score; //学生名が存在しない (新規格納)
             }
             return dict;
         }
