60d42cdad92abcefc4af106c6027a262507ba634
平澤　輝久 <OIBC34019@ojs.ac.jp>
2023-06-29 10:21:03 +0900

    Section03完成

--- TestProject/Test02/Program.cs
+++ TestProject/Test02/Program.cs
@@ -50,7 +50,7 @@ namespace Test02 {
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
         private static void Exercise03(List<int> numbers) {
-            
+            var ob = numbers.OrderBy(s => s);
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
